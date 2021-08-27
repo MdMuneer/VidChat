a9de78ecaff784ad989417e00951f11c95a89d1a
Muhammad Muneer <mdmuneer310@gmail.com>
2021-08-27 16:45:06 +0530

    updated chatbox component

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,19 +1,32 @@
 import { useState, useEffect } from "react";
 import { selectPeers, useHMSStore } from "@100mslive/hms-video-react";
 import Peer from "./Peer";
+import Chat from "./chat";
 import ButtonBar from "./ButtonBar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Room = () => {
   const peers = useHMSStore(selectPeers);
-  const [toggleChat, setToggleChat] = useState(true);
+ const [toggleChat, setToggleChat] = useState(true);
 
   return (
     <div className="grid-container" style={{ backgroundColor: "#1B2156" }}>
+
       <div className="peers-container">
         {peers.map((peer) => (
           <Peer key={peer.id} peer={peer} />
         ))}
+
+
+      </div>
+      <div className="chatbox-container">
+        <button
+          className="chatbox-btn"
+          onClick={() => setToggleChat(!toggleChat)}
+        >
+          In-call Messages
+        </button>
+        {toggleChat ? <Chat /> : null}
       </div>
     </div>
   );
