3bc550cff093266f6f9d0276789df9a2e5a02eeb
Muhammad Muneer <mdmuneer310@gmail.com>
2021-09-05 14:46:27 +0530

    mobile view upgrade

--- src/components/Room.js
+++ src/components/Room.js
@@ -3,31 +3,20 @@ import { selectPeers, useHMSStore } from "@100mslive/hms-video-react";
 import Peer from "./Peer";
 import Chat from "./chat";
 import ButtonBar from "./ButtonBar";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Room = () => {
+const Room = ({ toggleChat, setToggleChat }) => {
   const peers = useHMSStore(selectPeers);
- const [toggleChat, setToggleChat] = useState(true);
+  // const [toggleChat, setToggleChat] = useState(true);
 
   return (
     <div className="grid-container" style={{ backgroundColor: "#1B2156" }}>
-
       <div className="peers-container">
         {peers.map((peer) => (
           <Peer key={peer.id} peer={peer} />
         ))}
-
-
       </div>
-      <div className="chatbox-container">
-        <button
-          className="chatbox-btn"
-          onClick={() => setToggleChat(!toggleChat)}
-        >
-          In-call Messages
-        </button>
-        {toggleChat ? <Chat /> : null}
-      </div>
+      {/*<div className="screenshare">screenshare</div>*/}
+      <div className="chatbox-container">{toggleChat ? <Chat /> : null}</div>
     </div>
   );
 };
